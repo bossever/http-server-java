9e6824b6f23a3fecd8086dd7295dbef1cbec65d5
Ayushman Sachan <ashusachan01@gmail.com>
2024-07-27 22:39:27 +0530

    extract URL pathname from request

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,9 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,14 +11,36 @@ public class Main {
             // Since the tester restarts your program quite often, setting SO_REUSEADDR
             // ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
-
             Socket clientSocket = serverSocket.accept(); // Wait for connection from client.
             System.out.println("Accepted new connection");
-            clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
 
-            // responding with HTTP status 200
+            StringBuilder response = buildResponse(clientSocket);
+            System.out.println("Server responded with: " + response.toString());
+
+            clientSocket.getOutputStream().write(response.toString().getBytes());
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         }
+    }
+
+    private static StringBuilder buildResponse(Socket clientSocket) throws IOException {
+        InputStream input = clientSocket.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+        String urlPath = reader.readLine().split(" ")[1];
+
+        StringBuilder response = new StringBuilder();
+
+        if (urlPath.equals("/")) {
+            // add status to response
+            response.append("HTTP/1.1 200 OK\r\n");
+        } else {
+            response.append("HTTP/1.1 404 Not Found\r\n");
+        }
+        // add header to response
+        response.append("\r\n");
+        // add body to response
+        response.append("");
+        return response;
     }
 }
