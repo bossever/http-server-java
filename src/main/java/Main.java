3c7da69f37c304fcd78b737700343bc874b56512
Ayushman Sachan <ashusachan01@gmail.com>
2024-07-27 23:13:06 +0530

    Respond with body

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,40 +7,48 @@ import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) {
-        try (ServerSocket serverSocket = new ServerSocket(4221);) {
+        try (ServerSocket serverSocket = new ServerSocket(4221)) {
             // Since the tester restarts your program quite often, setting SO_REUSEADDR
             // ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
             Socket clientSocket = serverSocket.accept(); // Wait for connection from client.
             System.out.println("Accepted new connection");
 
-            StringBuilder response = buildResponse(clientSocket);
-            System.out.println("Server responded with: " + response.toString());
+            String response = buildResponse(clientSocket);
+            System.out.println("\nServer responded with: \n" + response);
 
-            clientSocket.getOutputStream().write(response.toString().getBytes());
+            clientSocket.getOutputStream().write(response.getBytes());
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         }
     }
 
-    private static StringBuilder buildResponse(Socket clientSocket) throws IOException {
+    private static String buildResponse(Socket clientSocket) throws IOException {
+        StringBuilder header = new StringBuilder();
+        String responseBody = "";
         InputStream input = clientSocket.getInputStream();
         BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-
-        String urlPath = reader.readLine().split(" ")[1];
+        String request = reader.readLine();
+        System.out.println("\nHTTP request received: \n" + request);
+        String urlPath = request.split(" ")[1];
 
         StringBuilder response = new StringBuilder();
 
         if (urlPath.equals("/")) {
             // add status to response
             response.append("HTTP/1.1 200 OK\r\n");
+        } else if (urlPath.matches(".*(/echo).*")) {
+            response.append("HTTP/1.1 200 OK\r\n");
+            responseBody = urlPath.split("/")[1];
+            header.append("Content-Type: text/plain\r\n");
+            header.append("Content-Length: ").append(responseBody.length()).append("\r\n");
         } else {
             response.append("HTTP/1.1 404 Not Found\r\n");
         }
         // add header to response
-        response.append("\r\n");
+        response.append(header).append("\r\n");
         // add body to response
-        response.append("");
-        return response;
+        response.append(responseBody).append("\r\n");
+        return response.toString();
     }
 }
