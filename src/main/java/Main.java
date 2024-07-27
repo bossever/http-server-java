be4acb3fdf4726fff0ebf152d99525a3828d1026
Ayushman Sachan <ashusachan01@gmail.com>
2024-07-27 21:44:45 +0530

    Respond with 200

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,21 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        System.out.println("Logs from your program will appear here!");
-
         try (ServerSocket serverSocket = new ServerSocket(4221);) {
             // Since the tester restarts your program quite often, setting SO_REUSEADDR
             // ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
 
-            serverSocket.accept(); // Wait for connection from client.
-            System.out.println("accepted new connection");
+            Socket clientSocket = serverSocket.accept(); // Wait for connection from client.
+            System.out.println("Accepted new connection");
+            PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+
+            // responding with HTTP status 200
+            writer.print("HTTP/1.1 200 OK\\r\\n\\r\\n\n");
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         }
