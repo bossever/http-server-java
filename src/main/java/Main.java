d11e51f3f837cd0f2e88cf23a1e56e9add446ec3
Ayushman Sachan <ashusachan01@gmail.com>
2024-08-03 22:09:39 +0530

    Added ExecutorService to enable concurrent connections

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import http.Client;
 
@@ -8,12 +10,18 @@ public class Main {
     private static final int PORT = 4221;
 
     public static void main(String[] args) {
-        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+        try (
+                ServerSocket serverSocket = new ServerSocket(PORT);
+                ExecutorService executorService = Executors.newCachedThreadPool();
+        ) {
             System.out.printf("Listening on port %d for connections...%n", PORT);
             serverSocket.setReuseAddress(true);
-            final Socket socket = serverSocket.accept();
-            final Client client = new Client(socket);
-            client.run();
+
+            while (true) {
+                final Socket socket = serverSocket.accept();
+                final Client client = new Client(socket);
+                executorService.submit(client);
+            }
         } catch (Exception e) {
             System.out.println("Exception: " + e.getMessage());
         }
