222445d00b589c263aa3edb6dbd145d93869e306
Ayushman Sachan <ashusachan01@gmail.com>
2024-08-08 13:18:32 +0530

    finish gzip compression support

--- src/main/java/http/encoding/Encoding.java
+++ src/main/java/http/encoding/Encoding.java
@@ -6,13 +6,22 @@ import java.io.IOException;
 import java.util.zip.GZIPOutputStream;
 
 public class Encoding {
-    public static byte[] encode(String encoding, byte[] body) throws IOException {
+    public static byte[] encode(String encoding, byte[] payload) throws IOException {
         if (encoding.equals("gzip")) {
-            ByteArrayInputStream inputStream = new ByteArrayInputStream(body);
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(payload);
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
             GZIPOutputStream gzipOutputStream = new GZIPOutputStream(outputStream);
+            byte[] buffer = new byte[512];
+            int bytesRead = 0;
 
-
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                gzipOutputStream.write(buffer, 0, bytesRead);
+            }
+            gzipOutputStream.finish();
+            byte[] compressedPayload = outputStream.toByteArray();
+            System.out.printf("GZIP compression finished, final payload size: %d bytes", compressedPayload.length);
+            return compressedPayload;
         }
+        return payload;
     }
 }
